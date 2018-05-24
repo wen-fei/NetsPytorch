cbcd2ad92ef5cbfa6a6835b941c3ce5274917ac7
wen-fei <282699766@qq.com>
2018-05-24 18:58:07 +0800

    AlexNet

---
+++ CNN/AlexNet.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python  
+# -*- coding: utf-8 -*-
+
+"""
+@author: TenYun  
+@contact: qq282699766@gmail.com  
+@time: 2018/5/24 13:59
+AlexNet,第一次引入了激活层ReLU，同时在全连接层引入了Dropout层防止过拟合
+5层卷积，3层全连接，共8层，使用了较大的滤波器11X11
+"""
+import torch
+from torch import nn
+from torch.autograd import Variable
+
+
+class AlexNet(nn.Module):
+    def __init__(self, num_classes):
+        super(AlexNet, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256*6*6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), 256*6*6)
+        x = self.classifier(x)
+        return x
+
